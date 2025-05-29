0b8d6613f7323fee7632831ab461f0172a2a3a85
Domirkhan <damirkhan252006@gmail.com>
2025-05-29 23:38:02 +0500

    hh

--- server/index.js
+++ server/index.js
@@ -35,11 +35,24 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000']; // Укажите ваш фронтенд-URL
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
